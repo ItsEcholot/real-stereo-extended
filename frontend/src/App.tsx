1bb30f330d21a3f239ade6b5c0a6b7de4d73870e
ItsEcholot <me@echolot.io>
2021-03-02 18:23:15 +0000

    Add react with ant.design for webapp

---
+++ frontend/src/App.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Layout } from 'antd';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <Layout>
+        <Layout.Header>
+          Header
+        </Layout.Header>
+        <Layout.Content>
+          Content
+        </Layout.Content>
+      </Layout>
+    </>
+  );
+}
+
+export default App;
