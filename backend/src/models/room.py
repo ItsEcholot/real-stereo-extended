964f363022c94b898dd1eca923eb6fa1f931c0a2
Cyril Wanner <info@cyr.li>
2021-04-26 12:06:36 +0000

    Store people_group in model

--- backend/src/models/room.py
+++ backend/src/models/room.py
@@ -10,15 +10,18 @@ class Room:
     :param int room_id: Room id
     :param str name: Name of the room
     :param List[model.node.Node] nodes: All nodes that are assigned to this room
+    :param str people_group: Strategy to calculate the coordinate in case of multiple people
     """
 
-    def __init__(self, room_id: int = None, name: str = '', nodes: List = None):
+    def __init__(self, room_id: int = None, name: str = '', nodes: List = None,
+                 people_group: str = ''):
         if len(name) == 0:
             raise ValueError('Room name cannot be empty')
 
         self.room_id: int = room_id
         self.name: str = name
         self.nodes: List[models.node.Node] = nodes or []
+        self.people_group: str = people_group
 
     @staticmethod
     def from_json(data: dict):
@@ -28,7 +31,7 @@ class Room:
         :returns: Room
         :rtype: Room
         """
-        return Room(data.get('id'), data.get('name'))
+        return Room(data.get('id'), data.get('name'), people_group=data.get('people_group'))
 
     def to_json(self, recursive: bool = False) -> dict:
         """Creates a JSON serializable object.
@@ -45,5 +48,8 @@ class Room:
 
         if recursive:
             json['nodes'] = list(map(lambda node: node.to_json(live=True), self.nodes))
+
+        if self.people_group is not None and len(self.people_group) > 0:
+            json['people_group'] = self.people_group
 
         return json
