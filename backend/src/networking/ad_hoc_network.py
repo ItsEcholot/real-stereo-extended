ff6babd355715b370952bd4d6c201ee312fd8c3d
Cyril Wanner <info@cyr.li>
2021-05-08 20:40:53 +0000

    Implement enabling/disabling the ad hoc network

---
+++ backend/src/networking/ad_hoc_network.py
@@ -0,0 +1,88 @@
+"""Handles the Ad Hoc Network used for initial setup and wifi configuration."""
+from os import system
+
+
+DHCP_CONFIG = '/etc/dhcpcd.conf'
+DISABLED_START_POINTER = '# --ad-hoc-start--\n'
+ENABLED_START_POINTER = '# --ad-hoc-start-enabled--\n'
+END_POINTER = '# --ad-hoc-end--\n'
+
+
+class AdHocNetwork:
+    """Handles the Ad Hoc Network used for initial setup and wifi configuration."""
+
+    def is_enabled(self) -> bool:
+        """Check if the ad hoc network is enabled.
+
+        :returns: True if the ad hoc network is enabled
+        :rtype: bool
+        """
+        with open(DHCP_CONFIG, 'r') as config_file:
+            config = config_file.readlines()
+            return ENABLED_START_POINTER in config
+
+    async def enable(self):
+        """Enables the ad hoc network"""
+        print('[AdHoc] Enabling the ad hoc network')
+
+        # enable the ad hoc network in the dhcp config
+        with open(DHCP_CONFIG, 'r') as config_file:
+            config = config_file.readlines()
+            if DISABLED_START_POINTER in config:
+                start = config.index(DISABLED_START_POINTER)
+                config[start] = ENABLED_START_POINTER
+                for i in range(start + 1, config.index(END_POINTER)):
+                    if config[i][0] == '#':
+                        config[i] = config[i][1:]
+
+                with open(DHCP_CONFIG, 'w') as write_handle:
+                    write_handle.writelines(config)
+
+                    # reload daemon config
+                    system('sudo systemctl daemon-reload')
+
+        if system('sudo service hostapd status > /dev/null') > 0:
+            # enable the hostapd service
+            system('sudo systemctl enable --now hostapd')
+
+            # restart the network
+            self.restart_network()
+
+            # restart the hostapd service to use the new dhcp config
+            system('sudo service hostapd restart')
+
+    def disable(self):
+        """Disable the ad hoc network"""
+        print('[AdHoc] Disabling the ad hoc network')
+
+        # disable the ad hoc network in the dhcp config
+        with open(DHCP_CONFIG, 'r') as config_file:
+            config = config_file.readlines()
+            if ENABLED_START_POINTER in config:
+                start = config.index(ENABLED_START_POINTER)
+                config[start] = DISABLED_START_POINTER
+                for i in range(start + 1, config.index(END_POINTER)):
+                    if config[i][0] != '#':
+                        config[i] = '#' + config[i]
+
+                with open(DHCP_CONFIG, 'w') as write_handle:
+                    write_handle.writelines(config)
+
+                    # reload daemon config
+                    system('sudo systemctl daemon-reload')
+
+        if system('sudo service hostapd status > /dev/null') < 1:
+            # disable the hostapd service
+            system('sudo systemctl disable --now hostapd')
+
+            # restart the network
+            self.restart_network()
+
+    def restart_network(self) -> None:
+        """Restarts the dhcp service and network interface."""
+        # restart the dhcp service
+        system('sudo service dhcpcd restart')
+
+        # restart the network interface
+        system('sudo ifconfig wlan0 down')
+        system('sudo ifconfig wlan0 up')
